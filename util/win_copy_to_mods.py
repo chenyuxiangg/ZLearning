9d7bef1e323b5f40d9caba828bf1b0327c3d0b5e
陈宇翔 <1007423165@qq.com>
2022-12-03 16:22:24 +0800

    update win_copy_to_mods.py

--- util/win_copy_to_mods.py
+++ util/win_copy_to_mods.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 git_root_path = "cyxblog_relate"
 git_mods_path = "DST\\mods"
@@ -9,5 +10,13 @@ def copy_allfile(srcdir, dstdir):
         print("please check two paths if exist or not:\n\t{}\n\t{}.".format(srcdir, dstdir))
         return False
     srcfiles = os.listdir(srcdir)
+    objcnt = 0
     for object in srcfiles:
-        test
+        object_path = os.path.join(srcdir, object)
+        if os.path.exists(object_path):
+            objcnt += 1
+            shutil.copy(object_path, dstdir)
+    print("from:\n\t{}\nto:\n\t{}".format(srcdir, dstdir))
+    print("copy files num: {}.".format(objcnt))
+
+copy_allfile(git_root_path+git_mods_path, local_mods_path)
