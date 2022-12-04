f21327ddb01cdc52d62152f114de96d7a039ee0e
陈宇翔 <1007423165@qq.com>
2022-12-04 15:35:06 +0800

    add position for rectanglewindow

--- DST/mods/ui_test/scripts/examples/template_rectanglewindow.lua
+++ DST/mods/ui_test/scripts/examples/template_rectanglewindow.lua
@@ -3,6 +3,7 @@ local TEMPLATES = require("widgets/redux/templates")
 local rectanglewindow = function(self)
     function self:ShowRectangleWindow()
         self.rectwindow = TEMPLATES.RectangleWindow(0, 0, "z_rect_templates", nil, nil, "from zyzs.")
+        self.rectwindow.SetPosition(300, 300)
         TheFrontEnd:PushScreen(self.rectwindow)
         return self.rectwindow
     end
@@ -14,8 +15,10 @@ local rectanglewindow = function(self)
         self.rectwindow = nil
     end
 
-    self.openbtn = self:AddChild(TEMPLATES.StandardButton(function() self:ShowRectangleWindow() end, "open", {100, 100}, nil))
-    self.closebtn = self:AddChild(TEMPLATES.StandardButton(function() self:CloseRectangelWindow() end, "close", {100, 150}, nil))
+    self.openbtn = self:AddChild(TEMPLATES.StandardButton(function() self:ShowRectangleWindow() end, "open", {100, 50}, nil))
+    self.closebtn = self:AddChild(TEMPLATES.StandardButton(function() self:CloseRectangelWindow() end, "close", {100, 50}, nil))
+    self.openbtn.SetPosition(100, 100)
+    self.closebtn.SetPosition(100,150)
 end
 
 return rectanglewindow
