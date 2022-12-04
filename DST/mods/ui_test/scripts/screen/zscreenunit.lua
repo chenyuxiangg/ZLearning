6a1b7fc350895539893a294a7e008132db1b8267
陈宇翔 <1007423165@qq.com>
2022-12-04 21:57:19 +0800

    update

--- DST/mods/ui_test/scripts/screen/zscreenunit.lua
+++ DST/mods/ui_test/scripts/screen/zscreenunit.lua
@@ -54,10 +54,10 @@ end
 
 function ZScreenUnit:OnRawKey(key, down)
     if ZScreenUnit._base.OnRawKey(self, key, down) then
-        print("_base " .. STRINGS.UI.CONTROLSSCREEN.INPUT[1][key] .. "is down.")
+        print("_base " .. STRINGS.UI.CONTROLSSCREEN.INPUTS[1][key] .. "is down.")
         return true
     else
-        print(INPUT[1][key] .. "is down.")
+        print(STRINGS.UI.CONTROLSSCREEN.INPUTS[1][key] .. "is down.")
         return true
     end
 end
