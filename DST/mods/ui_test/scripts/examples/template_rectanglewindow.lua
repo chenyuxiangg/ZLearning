927a626ce5d811b31f1edade89e84dddc18526a1
陈宇翔 <1007423165@qq.com>
2022-12-04 14:54:06 +0800

    add template for rectanglewindow

---
+++ DST/mods/ui_test/scripts/examples/template_rectanglewindow.lua
@@ -0,0 +1,21 @@
+local TEMPLATES = require("widgets/redux/templates")
+
+local rectanglewindow = function fn(self)
+    function self:ShowRectangleWindow()
+        self.rectwindow = TEMPLATES.RectangleWindow(0, 0, "z_rect_templates", nil, nil, "from zyzs.")
+        TheFrontEnd:PushScreen(self.rectwindow)
+        return self.rectwindow
+    end
+
+    function self:CloseRectangelWindow()
+        if self.rectwindow then
+            TheFrontEnd:PopScreen(self.rectwindow)
+        end
+        self.rectwindow = nil
+    end
+
+    self.openbtn = self:AddChild(TEMPLATES.StandardButton(function self:ShowRectangleWindow() end, "open", {100, 100}, nil))
+    self.closebtn = self:Addchild(TEMPLATES.StandardButton(function self:CloseRectangelWindow() end, "close", {100, 150}, nil))
+end
+
+return rectanglewindow
---
+++ DST/mods/ui_test/scripts/examples/template_screenroot.lua
@@ -0,0 +1,41 @@
+local TEMPLATES = require("widgets/redux/templates")
+
+local screenroot_ctor = function fn(self)
+    function self:ShowScreen()
+        self.screenroot = TEMPLATES.ScreenRoot()
+
+        function self.screenroot:OnBecomeActive()
+            TheSim:SetUIRoot(self.inst.entity)
+            if self.last_focus and self.last_focus.inst.entity:IsValid() then
+                self.last_focus:SetFocus()
+            else
+                self.last_focus = nil
+                if self.default_focus then
+                    self.default_focus:SetFocus()
+                end
+            end
+        end
+
+        function self.screenroot:OnBecomeInactive()
+            self.last_focus = self:GetDeepestFocus()
+        end
+
+        function self.screenroot:OnUpdate(dt)
+            return true
+        end
+            
+        function self.screenroot:OnDestroy()
+            self:Kill()
+        end
+
+        self.screenroot.bg = self.screenroot:AddChild(TEMPLATES.LoaderBackground("loading_pigking"))
+        self.screenroot.closebtn = self.screenroot:AddChild(TEMPLATES.StandardButton(function() TheFrontEnd:PopScreen(self.screenroot) end, "close", {100, 50}))
+        TheFrontEnd:PushScreen(self.screenroot)
+        return self.screenroot
+    end
+
+    self.openbtn = self:AddChild(TEMPLATES.StandardButton(function() self:ShowScreen() end, "open", {100, 50}))
+    self.openbtn:SetPosition(100,100)
+end
+
+return screenroot_ctor
