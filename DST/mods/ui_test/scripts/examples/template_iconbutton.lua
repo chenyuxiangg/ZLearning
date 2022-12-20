9b85de3851c16da3834e2a8f614731c2f8cb217c
陈宇翔 <1007423165@qq.com>
2022-12-20 21:21:58 +0800

    update

--- DST/mods/ui_test/scripts/examples/template_iconbutton.lua
+++ DST/mods/ui_test/scripts/examples/template_iconbutton.lua
@@ -3,8 +3,8 @@ local TEMPLATES = require("widgets/redux/templates")
 
 local iconbutton = function()
     local iconb = TEMPLATES.IconButton("images/button_icons.xml", "player_info.tex", "test ib", false, false, function() end)
-    local zs = ZS(self)
-    zs.left_root:AddChild(iconb)
+    local zs = ZS()
+    zs.right_root:AddChild(iconb)
     TheFrontEnd:PushScreen(zs)
     return zs
 end
