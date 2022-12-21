4add4c77d18833879edd8688a0fb7d2aa1124476
陈宇翔 <1007423165@qq.com>
2022-12-21 23:16:13 +0800

    update

--- DST/mods/ui_test/scripts/examples/template_showheader.lua
+++ DST/mods/ui_test/scripts/examples/template_showheader.lua
@@ -1,11 +1,9 @@
 local ZS = require("screen/zscreenunit")
-local GetAvatar = require("widgets/bantab").GetAvatar
-local GetAvatarAtlas = require("widgets/bantab").GetAvatarAtlas
 
 local header = function()
     local zs = ZS()
     local header_bg = zs.middle_root:AddChild(Image("images/avatars.xml", "avatar_bg.tex"))
-    local header_icon = zs.middle_root:AddChild(Image(GetAvatarAtlas("wilson", false), GetAvatar("wilson", false)))
+    local header_icon = zs.middle_root:AddChild(Image("images/avatars/avatar_wilson.xml", "avatar_wilson.tex"))
     local header_frame = zs.middle_root:AddChild(Image("images/avatars.xml", "avatar_frame_white.tex"))
     header_frame:SetTint(.5,.5,.5,1)
     TheFrontEnd:PushScreen(zs)
