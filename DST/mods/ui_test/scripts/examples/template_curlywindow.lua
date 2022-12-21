3f0005f6cc613a5d16a30a823cb804b2d9e79d54
陈宇翔 <1007423165@qq.com>
2022-12-21 19:36:02 +0800

    update

--- DST/mods/ui_test/scripts/examples/template_curlywindow.lua
+++ DST/mods/ui_test/scripts/examples/template_curlywindow.lua
@@ -2,9 +2,11 @@ local ZS = require("screen/zscreenunit")
 local TEMPLATES = require("widgets/redux/templates")
 
 local curlywin = function()
-    local win = TEMPLATES.CurlyWindow(100, 100, "win curly", nil, nil, "test")
     local zs = ZS()
-    zs.middle_root:AddChild(win)
+    local win = zs.middle_root:AddChild(TEMPLATES.CurlyWindow(100, 100, "win curly", nil, nil, "test"))
+    win.fill = zs.middle_root:AddChild(Image("images/fepanel_fills.xml", "panel_fill_tiny.tex"))
+    win.fill:SetScale(1.1, .9)
+    win.fill:SetPosition(8, 12)
     TheFrontEnd:PushScreen(zs)
     return zs
 end
