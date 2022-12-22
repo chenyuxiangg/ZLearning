7d414a7c6977bc5493d83df21e1e046bf17882bf
陈宇翔 <1007423165@qq.com>
2022-12-22 22:31:34 +0800

    update

--- DST/mods/ui_test/scripts/examples/template_votecontrol.lua
+++ DST/mods/ui_test/scripts/examples/template_votecontrol.lua
@@ -3,8 +3,8 @@ local ZS = require("screen/zscreenunit")
 local vote = function()
     local zs = ZS()
     local v = zs.topmiddle_root:AddChild(Image("images/ui.xml", "votewindow_controller_bottom.tex"))
-    v:SetPosition(0, -80, 0)
-    v:SetScale(2, 1, 1)
+    v:SetPosition(0, -50, 0)
+    v:SetScale(2, 1.5, 1)
     TheFrontEnd:PushScreen(zs)
     return zs
 end
