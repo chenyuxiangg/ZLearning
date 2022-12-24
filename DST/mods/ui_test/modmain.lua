384d3cda679d974a5249a5d89873077b8f729cf9
陈宇翔 <1007423165@qq.com>
2022-12-24 22:56:04 +0800

    update

--- DST/mods/ui_test/modmain.lua
+++ DST/mods/ui_test/modmain.lua
@@ -2,12 +2,13 @@ local fn = require "examples/template_votecontrol"
 local ZDEBUG = require("debug/debug")
 local _g = GLOBAL
 
-local function KillZscreenByKeyK(key, down)
+local function DealByKeyK(key, down)
     if not down then
         return
     end
     if _g.TheInput:IsKeyDown(_g.KEY_K) then
         local curscreen = TheFrontEnd:GetActiveScreen()
+        ZDEBUG:zprint(urscreen.name)
         if curscreen.name == "screenroot" and curscreen.votewindow ~= nil then
             ZDEBUG:zprint("clear votewindow focus.")
             curscreen.votewindow:ClearFocus()
@@ -16,7 +17,8 @@ local function KillZscreenByKeyK(key, down)
 end
 
 local function Simfn()
-    _g.TheInput:AddKeyHandler(KillZscreenByKeyK)
+    _g.TheInput:AddKeyHandler(DealByKeyK)
+    _g.TheInput:AddMouseButtonHandler()
 end
 
 AddSimPostInit(Simfn)
