5e1277aadfdef0f1915226c5d0444df7cf87cfe1
陈宇翔 <1007423165@qq.com>
2022-12-25 15:28:14 +0800

    update

--- DST/mods/ui_test/modmain.lua
+++ DST/mods/ui_test/modmain.lua
@@ -11,7 +11,7 @@ local function DealByKeyK(key, down)
     if _g.TheInput:IsKeyDown(_g.KEY_K) then
         local curscreen = TheFrontEnd:GetActiveScreen()
         ZDEBUG:zprint(curscreen.name)
-        if curscreen.name == "HUD" and not curscreen.votewindow.open then
+        if curscreen.name == "HUD" and curscreen.votewindow and not curscreen.votewindow.open then
             ZDEBUG:zprint("votewindow focus.")
             curscreen:OpenVote()
         else 
