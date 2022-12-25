8b54f818a58cdc78dd0696264f1eab70bb085bdb
陈宇翔 <1007423165@qq.com>
2022-12-25 17:15:22 +0800

    update

--- DST/mods/ui_test/scripts/examples/template_votecontrol.lua
+++ DST/mods/ui_test/scripts/examples/template_votecontrol.lua
@@ -12,39 +12,35 @@ local vote = function(self)
     function self:UpdateHeaderInfo(infotype, dir)
         if infotype == "x" then
             if dir == "add" then
-                self.votewindow.header_x = self.votewindow.header_x+1
+                self.votewindow.scrollbarline_x = self.votewindow.scrollbarline_x+1
             elseif dir == "sub" then
-                self.votewindow.header_x = self.votewindow.header_x-1
+                self.votewindow.scrollbarline_x = self.votewindow.scrollbarline_x-1
             end
         elseif infotype == "y" then
             if dir == "add" then
-                self.votewindow.header_y = self.votewindow.header_y+1
+                self.votewindow.scrollbarline_y = self.votewindow.scrollbarline_y+1
             elseif dir == "sub" then
-                self.votewindow.header_y = self.votewindow.header_y-1
+                self.votewindow.scrollbarline_y = self.votewindow.scrollbarline_y-1
             end
         elseif infotype == "sx" then
             if dir == "add" then
-                self.votewindow.header_sx = self.votewindow.header_sx+0.1
+                self.votewindow.scrollbarline_sx = self.votewindow.scrollbarline_sx+0.1
             elseif dir == "sub" then
-                self.votewindow.header_sx = self.votewindow.header_sx-0.1
+                self.votewindow.scrollbarline_sx = self.votewindow.scrollbarline_sx-0.1
             end
         elseif infotype == "sy" then
             if dir == "add" then
-                self.votewindow.header_sy = self.votewindow.header_sy+0.1
+                self.votewindow.scrollbarline_sy = self.votewindow.scrollbarline_sy+0.1
             elseif dir == "sub" then
-                self.votewindow.header_sy = self.votewindow.header_sy-0.1
+                self.votewindow.scrollbarline_sy = self.votewindow.scrollbarline_sy-0.1
             end
         end
-        self.votewindow.header_bg:SetPosition(self.votewindow.header_x, self.votewindow.header_y, 0)
-        self.votewindow.header_bg:SetScale(self.votewindow.header_sx, self.votewindow.header_sy, 0)
-        self.votewindow.header_icon:SetPosition(self.votewindow.header_x, self.votewindow.header_y, 0)
-        self.votewindow.header_icon:SetScale(self.votewindow.header_sx, self.votewindow.header_sy, 0)
-        self.votewindow.header_frame:SetPosition(self.votewindow.header_x, self.votewindow.header_y, 0)
-        self.votewindow.header_frame:SetScale(self.votewindow.header_sx, self.votewindow.header_sy, 0)
+        self.votewindow.scrollbarline:SetPosition(self.votewindow.scrollbarline_x, self.votewindow.scrollbarline_y, 0)
+        self.votewindow.scrollbarline:SetScale(self.votewindow.scrollbarline_sx, self.votewindow.scrollbarline_sy, 0)
     end
 
     function self:GetHeaderInfo()
-        print("x: ", self.votewindow.header_x, ", y: ", self.votewindow.header_y, ", sx: ", self.votewindow.header_sx, ", sy: ", self.votewindow.header_sy)
+        print("x: ", self.votewindow.scrollbarline_x, ", y: ", self.votewindow.scrollbarline_y, ", sx: ", self.votewindow.scrollbarline_sx, ", sy: ", self.votewindow.scrollbarline_sy)
     end
 
     self.ztopmiddle_root = self:AddChild(WIDGET("ztopmiddle_root"))
@@ -60,10 +56,15 @@ local vote = function(self)
     self.votewindow = self.ztopmiddle_root:AddChild(Image("images/ui.xml", "votewindow_controller_bottom.tex"))
     self.votewindow:SetScale(self.vote_scale_x, self.vote_scale_y, 1)
 
-    self.votewindow.header_x = -106
-    self.votewindow.header_y = 7.5
-    self.votewindow.header_sx = 0.5
+    self.votewindow.header_x = -113
+    self.votewindow.header_y = 10.5
+    self.votewindow.header_sx = 0.4
     self.votewindow.header_sy = 0.5
+
+    self.votewindow.scrollbarline_x = -110
+    self.votewindow.scrollbarline_y = 10.5
+    self.votewindow.scrollbarline_sx = 0.4
+    self.votewindow.scrollbarline_sy = 0.5
 
     self.votewindow.header_bg = self.votewindow:AddChild(Image("images/avatars.xml", "avatar_bg.tex"))
     self.votewindow.header_icon = self.votewindow:AddChild(Image("images/avatars.xml", "avatar_wilson.tex"))
@@ -74,6 +75,10 @@ local vote = function(self)
     self.votewindow.header_icon:SetScale(self.votewindow.header_sx, self.votewindow.header_sy, 0)
     self.votewindow.header_frame:SetPosition(self.votewindow.header_x, self.votewindow.header_y, 0)
     self.votewindow.header_frame:SetScale(self.votewindow.header_sx, self.votewindow.header_sy, 0)
+
+    self.votewindow.scrollbarline = self.votewindow:AddChild(Image("images/ui.xml", "scrollbarline.tex"))
+    self.votewindow.scrollbarline:SetPosition(self.votewindow.scrollbarline_x, self.votewindow.scrollbarline_y, 0)
+    self.votewindow.scrollbarline:SetScale(self.votewindow.scrollbarline_sx, self.votewindow.scrollbarline_sy, 0)
 
     self.votewindow.open = false
     self.votewindow:Hide()
