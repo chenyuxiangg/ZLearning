5e7ed30c61b2a47c70b3dc566e8f33722000fda2
陈宇翔 <1007423165@qq.com>
2022-12-28 23:44:31 +0800

    update

--- DST/mods/ui_test/modmain.lua
+++ DST/mods/ui_test/modmain.lua
@@ -5,10 +5,9 @@ local _g = GLOBAL
 
 local fn = function(self)
     self.ztopmiddle_root = self:AddChild(WIDGET("ZTopMiddleRoot"))
-    if self.ztopmiddle_root == nil then
-        print("add children failed.")
-        return
-    end
+    print("cyx")
+    ZDEBUG:zprint(self)
+    ZDEBUG:zprint(self.ztopmiddle_root)
     self.ztopmiddle_root:SetScaleMode(SCALEMODE_PROPORTIONAL)
     self.ztopmiddle_root:SetHAnchor(ANCHOR_MIDDLE)
     self.ztopmiddle_root:SetVAnchor(ANCHOR_TOP)
