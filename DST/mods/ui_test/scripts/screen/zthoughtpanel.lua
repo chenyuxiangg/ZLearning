5bc5df24965d658c4bdd9757d0b85356b320035d
陈宇翔 <1007423165@qq.com>
2022-12-29 21:33:13 +0800

    update

--- DST/mods/ui_test/scripts/screen/zthoughtpanel.lua
+++ DST/mods/ui_test/scripts/screen/zthoughtpanel.lua
@@ -8,9 +8,9 @@ local Zthoughtpanel = Class(Widget, function(self, owner)
     self.parent = owner
     self.open = false
     self.basePos = Vector3(0, -50, 0)
-    self.avatarPosOffset = Vector3(-240, 55, 0)
-    self.barLinePosOffset = Vector3(-214, 55, 0)
-    self.textLabelPosOffset = Vector3(-110, 65, 0)
+    self.avatarPosOffset = Vector3(-113, 65, 0)
+    self.barLinePosOffset = Vector3(-87, 65, 0)
+    self.textLabelPosOffset = Vector3(17, 65, 0)
 
     self.baseScale = Vector3(2, 1.5, 1)
     self.avatarScale = Vector3(0.5, 0.5, 1)
@@ -83,6 +83,7 @@ function Zthoughtpanel:getInfo()
     print("bg: ", self.bg:GetPosition())
     print("avatarBg: ", self.avatarBg:GetPosition())
     print("barLine: ", self.barLine:GetPosition())
+    print("textlable: ", self.textLable:GetPosition())
     print("font size: ", self.textLable:GetSize())
 end
 
